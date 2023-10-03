d056f03ca7804da090b0b18120b83f3071afdf43
madhuri yadav <126373490+YadavMayuri@users.noreply.github.com>
2023-10-04 01:08:28 +0530

    updates

--- frontend/src/Components/PopularMovies.jsx
+++ frontend/src/Components/PopularMovies.jsx
@@ -62,8 +62,11 @@ const PopularMovies = () => {
                 ) : (
                     <div className="  flex flex-wrap justify-between gap-2  ">
                         {movies.map((movie, index) => (
-                            <div className="mb-10 shadow-md rounded-md cursor-pointer border " key={index}  onClick={()=> router(`/singlepagedetails/${movie.id}`)}>
+                            <div className="mb-10 shadow-md rounded-md cursor-pointer border relative" key={index} onClick={() => router(`/singlepagedetails/${movie.id}`)}>
                                 <img src={`https://image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path} `} alt={movie.title} className="w-[10rem] h-[14rem] shadow-sm rounded-md" />
+                                <div className="absolute top-[200px] left-3 bg-sky-950 text-xs font-semibold text-white rounded-full w-9 h-9 flex items-center justify-center">
+                                {(movie.vote_average * 10).toFixed()}%
+                                </div>
                                 <div className="flex flex-col w-[10rem] px-2 py-3">
                                     <h1 className="font-bold hover:text-cyan-500">{movie.title}</h1>
                                     <p className="font-normal text-slate-400 text-sm">{movie.release_date}</p>
